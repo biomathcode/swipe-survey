35d59102f874b543bb961e6c01ad02f045cd15bb
Pratik sharma <sharma.pratik2016@gmail.com>
2023-07-13 03:32:02 +0530

    fix: update with api

--- src/pages/api/response/[id].ts
+++ src/pages/api/response/[id].ts
@@ -1,3 +1,24 @@
 //GET:  get response
 //DELETE:  delete response
 //UPDATE:  udpate response
+
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../lib/prisma";
+import { Question } from "@prisma/client";
+
+export default async function Response(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const id = req.query.id as string;
+
+  if (req.method === "GET") {
+    const question = await prisma.question.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    res.status(200).json({ data: question, msg: "SuccessFul " });
+  }
+}

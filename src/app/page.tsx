a90af5923983032076ad9a384268642068cdd182
Pratik sharma <sharma.pratik2016@gmail.com>
2023-07-13 14:54:12 +0530

    added frontend swipe component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,6 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white text-black ">
       <Header />
       <div> Swipe here</div>
-
-      <Swiper />
     </main>
   );
 }

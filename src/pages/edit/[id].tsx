ba128a258247368379c501c4cc31fa268f0ba740
Pratik sharma <sharma.pratik2016@gmail.com>
2023-07-17 16:02:28 +0530

    fix: added swiper to the client componet

--- src/pages/edit/[id].tsx
+++ src/pages/edit/[id].tsx
@@ -19,29 +19,32 @@ function Edit(props: any) {
   console.log(newData);
   return (
     <>
-      <div
-        style={{ width: "100vw", height: "calc(100vh - 50px" }}
-        className="mt-20 flex justify-center items-center content-center"
-      >
-        <div className="flex flex-col min-h-full max-w-lg ">
-          <input
-            className="text-6xl border-b-2 border-gray-600"
-            defaultValue={props.data.title}
-          />
-          <QuestionForm
-            surveyid={props.data.id}
-            data={data}
-            setData={setData}
-          />
+      <div className="flex justify-around">
+        <div
+          style={{ width: "50vw", height: "calc(100vh - 50px" }}
+          className="mt-20 flex justify-center items-center content-center"
+        >
+          <div className="flex flex-col min-h-full max-w-lg ">
+            <input
+              className="text-6xl border-b-2 border-gray-600"
+              defaultValue={props.data.title}
+            />
+            <QuestionForm
+              surveyid={props.data.id}
+              data={data}
+              setData={setData}
+            />
 
-          {data.map((el, i) => (
-            <EditForm i={i} el={el} key={i} />
-          ))}
+            {data.map((el, i) => (
+              <EditForm i={i} el={el} key={i} />
+            ))}
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl font-bold text-center">Preview</h1>
+          <Swiper data={props.data} />
         </div>
       </div>
-      {/* <div style={{ width: "50vw", height: "100vh" }}> */}
-      <Swiper data={props.data} />
-      {/* </div> */}
     </>
   );
 }

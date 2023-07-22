765daf096622c563efe04fdd63092d63a40e18d4
Pratik sharma <sharma.pratik2016@gmail.com>
2023-07-23 00:37:24 +0530

    added: landing page usecase

--- src/component/Usecase/data.tsx
+++ src/component/Usecase/data.tsx
@@ -1,6 +1,38 @@
-// Should your company pivot?
-// 10 Questions to ask before your buys something
-//
-const data = [{}];
+const data = [
+  {
+    id: 1,
+    header: "Startup",
+    description:
+      "Get insights of your data, remove outliers and get a proper path to your hypothesis",
+  },
+  {
+    id: 2,
+    header: "Product Feedback",
+    description:
+      "Polarised response create a more definite path which leads to Decision Making",
+  },
+  {
+    id: 3,
+    header: "Hypothesis Testing",
+    description:
+      "Add Swipe Survey to your website with our embed, generate link ",
+  },
+  {
+    id: 4,
+    header: "Decision Validation",
+    description:
+      "Decision Validation Services is designed for developers, QA teams, and business users to create testing and simulation solutions based on a number of use cases and scenarios",
+  },
+  {
+    id: 5,
+    header: "S ",
+    description: "It's simple to create, collect and get conclusive data.  ",
+  },
+  {
+    id: 6,
+    header: "Larger Sample Size",
+    description: "Companies should increase the sample size of their surveys. ",
+  },
+];
 
 export default data;

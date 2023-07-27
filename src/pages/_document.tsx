5a4a5f66f4fdfe92b726ef07690d47e0ceed8f43
Pratik sharma <sharma.pratik2016@gmail.com>
2023-07-27 13:57:45 +0530

    added clarity

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "i5rd3b4gkx");
+        
+        `,
+          }}
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
